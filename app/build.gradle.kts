9b26c9862dfcb6b4043d907c6de5fc019b2f2995
alhmeemzool-ops <alhmeemzool@gmail.com>
2026-08-26 17:47:53 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
     }
     kotlinOptions { jvmTarget = "17" }
 }
-dependencies {
+dependencies {implementation("androidx.lifecycle:lifecycle-service:2.7.0")
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
