22dc7c040d67ae0115aa5d588abc2c4cab46302a
alhmeemzool-ops <alhmeemzool@gmail.com>
2026-08-26 06:51:49 +0200

    Update compileSdk and targetSdk to version 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 android {
     namespace = "com.hamam.quickocr"
-    compileSdk = 35
+    compileSdk = 36
     defaultConfig {
         applicationId = "com.hamam.quickocr"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
